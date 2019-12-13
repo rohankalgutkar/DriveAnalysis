9ef82015edd855856175a27d7554f625e48ed82a
rohankalgutkar@gmail.com <rohankalgutkar@gmail.com>
2019-12-14 01:15:20 +0530

    Form data to BE and create CSV

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+const moment = require('moment');
+
 const app = express()
 const port = process.env.PORT || 3000
-app.use(express.static('public'));
+const dateFormat = 'DD/MM/YYYY'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    'extended': true
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html/index.html'))
+    // res.send('Hello World!')
+})
+
+app.post('/trip', (req, res) => {
+    let {
+        km,
+        message
+    } = req.body
+
+    let trip = [moment(new Date()).format(dateFormat), km, message]
+
+    fs.appendFileSync('trips.csv', trip + os.EOL)
+    res.send('Trip Added')
+})
+
+app.post('/fuel', (req, res) => {
+    let {
+        amt,
+        ltr
+    } = req.body
+
+    let fuelup = [moment(new Date()).format(dateFormat), ltr, amt, amt / ltr]
+
+    fs.appendFileSync('fuelup.csv', fuelup + os.EOL)
+    res.send('Fuel-up Added')
+})
+
+app.get('*', (req, res) => {
+    res.send('404')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
